7dbed42eb9703e3bd5e9bf14d0cea2d9419c9958
Aya Shabana <59667304+aya444@users.noreply.github.com>
2024-09-05 19:24:10 +0300

    first commit

---
+++ consumer/src/main/java/com/aya/wikimedia/consumer/config/WikimediaKafkaTopicCofig.java
@@ -0,0 +1,17 @@
+package com.aya.wikimedia.consumer.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class WikimediaKafkaTopicCofig {
+
+    @Bean
+    public NewTopic wikimediastreamTopic() {
+        return TopicBuilder
+                .name("wikimedia-stream")
+                .build();
+    }
+}
